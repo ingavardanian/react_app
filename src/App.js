2f1a9013b9bff8ea605ccf452e6f0e47bfe6cf39
ingvardanian <ingavardanyan0309@mail.ru>
2023-01-30 15:18:14 +0400

    change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>{value}</h1>
+     <input type = "text" value ={value} onChange={(evt)=> {
+      setValue(evt.target.value);
+     }}/>
     </div>
   );
+
 }
 
 export default App;
