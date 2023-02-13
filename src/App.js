9b8179d8afdf3df7f147b38d8c854812a0713d24
ingvardanian <ingavardanyan0309@mail.ru>
2023-02-13 14:11:12 +0400

    form changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
 import {useState} from "react";
 import './App.css';
+import TodoFooter from "./TodoFooter";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
 
 function App() {
-  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState([
+     {
+      id: Math.random(),
+      text:"Learn JS",
+      isCompleted: false
+     },
+     {
+      id: Math.random(),
+      text: "Learn CSS",
+      isCompleted: false
+     },
+     {
+      id: Math.random(),
+      text: "Learn react",
+      isCompleted: false
+     }
+  ]);
   return (
     <div className="App">
-      
-      <h1>{value}</h1>
-     <input type = "text" value ={value} onChange={(evt)=> {
-      setValue(evt.target.value);
-     }}/>
+      <TodoForm onAdd = {(text)=> {
+        setTodos([
+          ...todos,
+          {
+            id:Math.random(),
+            text: text,
+            isCompleted: false, 
+          }
+        ])
+      }}/>
+      <TodoList 
+        todos = {todos}
+        onDelete = {(todo)=> {
+          setTodos(todos.filter((t)=> t.id !== todo.id));
+        }}
+        onChange = {(newTodo)=>{
+          setTodos(todos.map((todo)=>{
+            if(todo.id === newTodo.id){
+              return newTodo;
+            }
+            return todo;
+          }));
+        }}
+        />
+      <TodoFooter todos = {todos} onClearCompleted = {()=> {
+       setTodos(todos.filter((todo) => !todo.isCompleted));
+      }}/>
     </div>
   );
 
